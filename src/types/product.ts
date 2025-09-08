ffaac356a396a1707a664209a05419130d3fbfce
lisafolawiyo <services.lisafolawiyo@gmail.com>
2025-09-08 11:20:01 +0100

    Add files via upload

--- src/types/product.ts
+++ src/types/product.ts
@@ -8,3 +8,85 @@
       name: string;
       alt: string;
     };
+
+  export type ProductType = {
+    id: number;
+    name: string;
+    slug: string;
+    type: string;
+    status: string;
+    description: string;
+    short_description: string;
+    sku: string;
+    price: string;
+    regular_price: string;
+    sale_price: string;
+    on_sale: boolean;
+    upsell_ids: number[];
+    categories: {
+      id: number;
+      name: string;
+      slug: string;
+    }[];
+    tags: {
+      id: number;
+      name: string;
+      slug: string;
+    }[];
+    images: {
+      id: number;
+      date_created: string;
+      date_created_gmt: string;
+      date_modified: string;
+      date_modified_gmt: string;
+      src: string;
+      name: string;
+      alt: string;
+    }[];
+    attributes: {
+      id: number;
+      name: string;
+      slug: string;
+      position: number;
+      visible: boolean;
+      variation: boolean;
+      options: string[];
+    }[];
+    variations: number[];
+    related_ids: number[];
+    meta_data: {
+      id: number;
+      key: string;
+      value: string;
+    }[];
+    stock_status: string;
+  };
+  
+export type ProductVariation = {
+  id: number;
+  type: string;
+  price: string;
+  regular_price: string;
+  sale_price: string;
+  on_sale: boolean;
+  status: string;
+  stock_quantity: number | null;
+  image: {
+    id: number;
+    date_created: string;
+    date_created_gmt: string;
+    date_modified: string;
+    date_modified_gmt: string;
+    src: string;
+    name: string;
+    alt: string;
+  };
+  attributes: {
+    id: number;
+    name: string;
+    slug: string;
+    option: string;
+  }[];
+  name: string;
+  parent_id: number;
+};

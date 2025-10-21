f686ca5119ce6a19b4b884d78f1845dbc31d9ef7
lisafolawiyo <services.lisafolawiyo@gmail.com>
2025-10-21 21:07:34 +0100

    chore: update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,38 +1,39 @@
 import type { Metadata } from "next";
-import localFont from 'next/font/local';
+import localFont from "next/font/local";
 import "./styles/globals.scss";
-import { GoogleAnalytics } from '@next/third-parties/google';
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const centuryGothic = localFont({
   src: [
     {
-      path: './fonts/CenturyGothic.woff2',
-      weight: '400',
-      style: 'normal',
+      path: "./fonts/CenturyGothic.woff2",
+      weight: "400",
+      style: "normal",
     },
     {
-      path: './fonts/CenturyGothic-Bold.woff2',
-      weight: '600',
-      style: 'normal',
+      path: "./fonts/CenturyGothic-Bold.woff2",
+      weight: "600",
+      style: "normal",
     },
     {
-      path: './fonts/CenturyGothic-Italic.woff2',
-      weight: '400',
-      style: 'italic',
+      path: "./fonts/CenturyGothic-Italic.woff2",
+      weight: "400",
+      style: "italic",
     },
     {
-      path: './fonts/CenturyGothic-BoldItalic.woff2',
-      weight: '600',
-      style: 'italic',
+      path: "./fonts/CenturyGothic-BoldItalic.woff2",
+      weight: "600",
+      style: "italic",
     },
   ],
-  variable: '--font-century-gothic',
-  display: 'swap',
-})
+  variable: "--font-century-gothic",
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Lisa Folawiyo | Luxury African Fashion with Hand Embellishment",
-  description: "Discover Lisa Folawiyo's luxury African fashion, where hand embellishment meets contemporary elegance. Shop our latest collections today.",
+  description:
+    "Discover Lisa Folawiyo's luxury African fashion, where hand embellishment meets contemporary elegance. Shop our latest collections today.",
 };
 
 export default function RootLayout({
@@ -43,7 +44,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${centuryGothic.variable}  antialiased`}>
-            {children}
+        {children}
       </body>
       <GoogleAnalytics gaId="G-M03YSEG0D9" />
     </html>

d30646095bfda4c02a88cf086062c863ce24401f
lisafolawiyo <services.lisafolawiyo@gmail.com>
2025-11-04 22:24:03 +0100

    chore: setup social icons

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -158,3 +158,34 @@ export const TwitterX = (props: SVGProps<SVGSVGElement>) => (
     </defs>
   </svg>
 );
+
+export const FacebookWhite = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    width={40}
+    height={40}
+    viewBox="0 0 40 40"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    {...props}
+  >
+    <rect width={40} height={40} fill="url(#pattern0_668_820)" />
+    <defs>
+      <pattern
+        id="pattern0_668_820"
+        patternContentUnits="objectBoundingBox"
+        width={1}
+        height={1}
+      >
+        <use xlinkHref="#image0_668_820" transform="scale(0.0111111)" />
+      </pattern>
+      <image
+        id="image0_668_820"
+        width={90}
+        height={90}
+        preserveAspectRatio="none"
+        xlinkHref="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAFoAAABaCAYAAAA4qEECAAAACXBIWXMAAAsTAAALEwEAmpwYAAACX0lEQVR4nO3cvWoWURCH8amMBES0jZWNgYCInXdgZ+EtWBqtxEuwttZKEG/BTrHwo1Ir0d5WkQS08gkrC28CB/asZ3dmyPn/+jAzDy+bD8iaiYiIiIiIiIiIAWeB+8AH4JDT6xB4D9wDtrwjXwI+059Pw+2en+QeIx+Pvf4ne3xc9G7fI/TwTO7dO4/QB9FXJnDgEVoAhXai0E4U2olCO1FoJwq98QN4BtwBrgE7wPZ4wwXgInAZ2ANuAi+YQaH5F/gBcG7mXcPXVFuv8GahzL4Cu/95l0JX+gacb7hLoSv8Aa433qXQFR4vcJdCT/gLXFngLoWe8GahuxR6wqOF7lLoCbcq974BvBx/zm7WY+i9ip2H3wx/Lzm0x9A7FTs/X3poj6G3K3b+svTQ7kJb3c4/I+b2GHpxCl0QNbcJyVjQzgpdEDW3yRpLhx1z8q6nqXbDkTkCXqfabb2sAcecvOs7M3gs5MacDL/0jH9uzbPbelkDjtncdJWZPJZyY06A2+l2Wydp0DGbmx4yk8dSbswJ8CTdbuskDTpmc9MrZvJYKhUL2lmhC6LmNiEZC9pZoQui5jYhGQvaWaELouY2IRkL2lmhC6LmNiEZC9pZoQui5jYhGQvaWaELouY2IRkL2lmhC6LmNiEZC9pZoQui5jYhGQvaWaELouY2IRkL2lmhC6LmNiEZO8WhU72OzWJC/+ruBYMWE9rlBYPDW2d7D33XI/TW+CLUXkN/BM6sHvrYa41TxLa6fZeMPPkPpGt8sveH51XkN0ir27XFcNvb4XHh9kkWEREREREREbHcjgD5oeFMZ1mogQAAAABJRU5ErkJggg=="
+      />
+    </defs>
+  </svg>
+);
